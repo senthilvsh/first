66d6986bc39ed5d88b83858a2191ba49c9509f49
Senthil Kumar <273322018+senthilvsh@users.noreply.github.com>
2026-04-03 17:08:45 +0530

    Use a for-loop

--- Main.cpp
+++ Main.cpp
@@ -4,9 +4,8 @@ int main()
 {
 	std::cout << "First v0.1\n";
 
-	int i = 1;
-	while (i <= 10) {
-		std::cout << i++ << "\n";
+	for (int i = 1; i <= 10;i++) {
+		std::cout << i << "\n";
 	}
 
 	return 0;
